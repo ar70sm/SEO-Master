400cad3d63063b203d272bfda2a3b1af37e8f7b4
Abdallah Ashraf <ar70sm@gmail.com>
2023-03-09 16:24:20 +0200

    The initial construction of the page

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
 // *********************** navScroll
 let nav = document.getElementsByTagName("nav")[0];
+let toUp = document.getElementById("toUp");
 
 addEventListener("scroll", navScroll);
 navScroll();
 
 // the function
 function navScroll() {
-  if (window.pageYOffset > 80) nav.classList.add("scroll");
-  else nav.classList.remove("scroll");
+  if (window.pageYOffset > 80){
+    nav.classList.add("scroll");
+    toUp.style.opacity = 1
+  }
+  else{
+    nav.classList.remove("scroll");
+    toUp.style.opacity = 0
+  }
 }
 
 // *********************** dropdown list
@@ -51,3 +58,39 @@ function scrollAnimation(ele) {
     ele.style.animationName = ele.dataset.sa
   }
 }
+
+
+// Projects
+
+let projectClasses = [...document.querySelectorAll("#projects li")]
+let projects = [...document.querySelectorAll("#projects .box")]
+
+projectClasses.forEach(e=>{
+  e.addEventListener("click",()=>{
+    projectClasses.forEach(e=>{
+      e.classList.remove("active")
+    })
+    e.classList.add("active")
+    projects.forEach(ele=>{
+        if ( ele.classList.contains(e.dataset.class)||e.dataset.class=="all"){
+          ele.style.display = "block"
+        }
+        else{
+          ele.style.display = "none"
+        }
+      })
+  })
+})
+// owl carousel
+
+$('.owl-carousel').owlCarousel({
+    items:3,
+    center:true,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    smartSpeed:1500,
+    dots:false
+});
